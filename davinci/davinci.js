67833eee3ed3481738f28b653fcaf22f75ab9a19
usman-faisal <usman.shamsi987@gmail.com>
2023-06-08 04:40:58 +0500

    fixed image generation and left with error handling and design improvements

--- davinci/davinci.js
+++ davinci/davinci.js
@@ -3,69 +3,51 @@ const button = document.querySelector("#send-btn");
 const inputText = document.querySelector("#text");
 
 const outputBox = document.querySelector(".output-box");
+let images;
+let currentImageIndex = 0;
 
+const updateImageUrl = () => {
+  document.querySelector("img").src = images[currentImageIndex];
+}
 button.addEventListener("click", async () => {
   const img = document.querySelector("img");
-  console.log(img);
-  if (img) img.remove();
-  const encodedParams = new URLSearchParams();
-  encodedParams.set("prompt", inputText.value);
-  encodedParams.set("id", "12345");
-  encodedParams.set("width", "768");
-  encodedParams.set("height", "768");
-  encodedParams.set("inferenceSteps", "50");
-  encodedParams.set("guidanceScale", "7.5");
-  encodedParams.set("img2img_strength", "0.75");
+  if(img) img.remove();
 
   const options = {
-    method: "POST",
-    url: "https://arimagesynthesizer.p.rapidapi.com/generate",
+    method: 'POST',
+    url: 'https://ai-image-generator3.p.rapidapi.com/generate',
     headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      "X-RapidAPI-Key": "5a23baae9emsh9c14d082a1ef12ep1b2b30jsn93ff59c2bf6f",
-      "X-RapidAPI-Host": "arimagesynthesizer.p.rapidapi.com",
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '5a23baae9emsh9c14d082a1ef12ep1b2b30jsn93ff59c2bf6f',
+      'X-RapidAPI-Host': 'ai-image-generator3.p.rapidapi.com'
     },
-    data: encodedParams,
+    data: {
+      prompt: inputText.value,
+      page: 1
+    }
   };
+
   try {
     const response = await axios.request(options);
-    const hash = response.data.hash;
-
-    const options1 = {
-      method: "GET",
-      url: "https://arimagesynthesizer.p.rapidapi.com/get",
-      params: {
-        hash: hash,
-        returnType: "image",
-      },
-      responseType: "arraybuffer",
-      headers: {
-        "X-RapidAPI-Key": "5a23baae9emsh9c14d082a1ef12ep1b2b30jsn93ff59c2bf6f",
-        "X-RapidAPI-Host": "arimagesynthesizer.p.rapidapi.com",
-      },
-    };
-
-    try {
-      let response = await axios.request(options1);
-      if (response.data.message) {
-        while (true) {
-          setTimeout(async () => {
-            const newResponse = await axios.request(options1);
-            response = newResponse;
-          }, 5000);
-        }
+    const newImg = document.createElement("img");
+    images = response.data.results.images;
+    const imageUrl = response.data.results.images[currentImageIndex];
+    newImg.src = imageUrl;
+    newImg.classList.add("img");
+    const button = document.createElement("button");
+    button.textContent = "Generate another variety";
+    button.classList.add("button");
+    button.addEventListener("click",() => {
+      currentImageIndex ++;
+      if(currentImageIndex > images.length){
+        return alert("no more varieties");
       }
-      const base64Data = btoa(
-        String.fromCharCode(...new Uint8Array(response.data))
-      );
-      const img = document.createElement("img");
-      img.classList.add("img");
-      img.src = "data:image/png;base64," + base64Data;
-      outputBox.append(img);
-    } catch (error) {
-      console.error(error);
-    }
+      updateImageUrl();
+    })
+    outputBox.append(button);
+    outputBox.append(newImg);
   } catch (error) {
     console.error(error);
   }
 });
+
